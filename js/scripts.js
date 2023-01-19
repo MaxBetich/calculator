381842503c676ef1092a63d62925a844cbe03b23
Max Betich <betichm@gmail.com>
2023-01-18 20:10:42 -0800

    add subtraction, multiplication, division functionality

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+function add(number1, number2)  {
+  return number1 + number2;
+}
+
+function subtract(number1, number2) {
+  return number1 - number2;
+}
+
+function multiply(number1, number2) {
+  return number1  * number2;
+}
+
+function divide(number1, number2) {
+  return number1 / number2;
+}
+
+const number1 = parseInt(prompt("Enter a number:"));
+const number2 = parseInt(prompt("Enter another number:"));
+
+window.alert("The sum of your numbers equals: " 
+  + add(number1,number2) 
+  + ". The subtraction of your numbers equals: " 
+  + subtract(number1, number2)  
+  + ". The multiplication of  your  numbers equals: " 
+  + multiply(number1, number2) 
+  + ". The division of your numbers equals: " 
+  + divide(number1, number2) 
+  + ".");

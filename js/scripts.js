c4e24c57759f4185f45fcf3ff95dfcf83d49a31c
Max Betich <betichm@gmail.com>
2023-03-20 14:56:52 -0700

    edit js output scripts

--- js/scripts.js
+++ js/scripts.js
@@ -17,12 +17,12 @@ function divide(number1, number2) {
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
 
-window.alert("The sum of your numbers equals: " 
+window.alert("The sum of " + number1 + " and " + number2 +" equals: " 
   + add(number1,number2) 
-  + ". The subtraction of your numbers equals: " 
+  + ". The subtraction of " + number1 + " and " + number2 + " equals: " 
   + subtract(number1, number2)  
-  + ". The multiplication of  your numbers equals: " 
+  + ". The multiplication of " + number1 + " and " + number2 + " equals: " 
   + multiply(number1, number2) 
-  + ". The division of your numbers equals: " 
+  + ". The division of " + number1 + " and " + number2 + " equals: " 
   + divide(number1, number2) 
   + ".");
